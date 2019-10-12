ce735c65d47e625425e7b70bb7685e8e47ba957a
Aaron Jackson <aarontjackson@outlook.com>
2019-10-12 14:18:16 -0400

    creation of orm.js file with methods in config directory

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,57 @@
 var connection = require('../config/connection.js');
 
-findAll()
+// object for all our SQL statement functions
+var orm = {
+findAll: function(tableName, cb){
+    var sqlQuery = "SELECT * FROM ??";
+    var data = [tableName];
+    connection.query(sqlQuery, data, function(err, result){
+        if (err){
+            throw err;
+        }
+        cb(result);
+    })
+},
 
-insertOne()
+insertOne: function(table, cols, vals, cb) {
+    var sqlQuery = "INSERT INTO ?? ("
+        sqlQuery += cols.toString() + ") "
+        sqlQuery += "VALUES ("
+        sqlQuery += printQMarks(vals) + ")"
 
-updateOne()
+        vals.unshift(tableName)
+
+    connection.query(sqlQuery, vals, function(err, result){
+        cb(result)
+    })
+},
+
+updateOne: function(table, objcolvals, condition, cb) {
+    var sqlQuery = "UPDATE ?? ("
+    sqlQuery += " SET ("
+    sqlQuery += objtoSql(objcolvals) + ") "
+    sqlQuery += "WHERE ("
+    sqlQuery += condition + ")"
+
+    vals.unshift(tableName)
+
+connection.query(sqlQuery, function(err, result) {
+    if(err) {
+        throw err;
+    }
+
+    cb(result);
+});
+}
+
+};
+
+function printQMarks(vals) {
+    var newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push('?')
+    }
+    return newArr.toString()
+}
+
+module.exports = orm;
